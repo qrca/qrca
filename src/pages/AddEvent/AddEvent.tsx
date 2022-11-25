63f480d1508255e12d5b9261b04e70a185842290
H-isaac23 <dev.isaac23@gmail.com>
2022-11-25 08:29:53 +0800

    finish styles on addEvent

--- src/pages/AddEvent/AddEvent.tsx
+++ src/pages/AddEvent/AddEvent.tsx
@@ -1,15 +1,13 @@
 import {
   IonInput,
   IonItem,
-  IonLabel,
   IonList,
   IonPage,
   IonContent,
   IonSelect,
   IonSelectOption,
   IonButton,
-  IonDatetime,
-  IonModal,
+  IonLabel,
 } from "@ionic/react";
 import { useState } from "react";
 
@@ -21,9 +19,6 @@ const AddEvent = () => {
     console.log(e.target.value);
   };
 
-  const [isModalOneOpen, setIsModalOneOpen] = useState(false);
-  const [isModalTwoOpen, setIsModalTwoOpen] = useState(false);
-  const [isModalThreeOpen, setIsModalThreeOpen] = useState(false);
   // const d;
 
   return (
@@ -31,79 +26,28 @@ const AddEvent = () => {
       <Header />
       <IonContent class="custom-add-event">
         <IonList class="custom-list">
+          <h1>Add Event</h1>
           <IonItem>
-            <IonLabel position="stacked">Event type</IonLabel>
             <IonSelect interface="action-sheet" placeholder="Select event type">
               <IonSelectOption value="Major">Major</IonSelectOption>
               <IonSelectOption value="Minor">Minor</IonSelectOption>
             </IonSelect>
           </IonItem>
           <IonItem>
-            <IonLabel position="stacked">Event name</IonLabel>
-            <IonInput class="custom" placeholder="Enter event name"></IonInput>
+            <IonInput placeholder="Enter event name"></IonInput>
           </IonItem>
           <IonItem>
-            <IonLabel position="stacked">Date</IonLabel>
-            <IonInput
-              placeholder="Date"
-              onIonFocus={() => setIsModalOneOpen(true)}
-            ></IonInput>
-            <IonModal isOpen={isModalOneOpen}>
-              <div className="datetime-picker">
-                <IonButton
-                  expand="block"
-                  onClick={() => setIsModalOneOpen(false)}
-                >
-                  Close
-                </IonButton>
-                <IonDatetime
-                  onIonChange={(e) => getTime(e)}
-                  presentation="date"
-                ></IonDatetime>
-              </div>
-            </IonModal>
+            <IonInput placeholder="Date" type="date"></IonInput>
           </IonItem>
           <IonItem>
-            <IonLabel position="stacked">Start time</IonLabel>
-            <IonInput
-              placeholder="Start time"
-              onIonFocus={() => setIsModalTwoOpen(true)}
-            ></IonInput>
-            <IonModal isOpen={isModalTwoOpen}>
-              <div className="datetime-picker">
-                <IonButton
-                  expand="block"
-                  onClick={() => setIsModalTwoOpen(false)}
-                >
-                  Close
-                </IonButton>
-                <IonDatetime
-                  onIonChange={(e) => getTime(e)}
-                  presentation="time"
-                ></IonDatetime>
-              </div>
-            </IonModal>
+            <IonInput placeholder="Start time" type="time"></IonInput>
           </IonItem>
           <IonItem>
-            <IonLabel position="stacked">End time</IonLabel>
             <IonInput
               placeholder="End time"
-              onIonFocus={() => setIsModalThreeOpen(true)}
+              type="time"
+              onIonChange={(e) => getTime(e)}
             ></IonInput>
-            <IonModal isOpen={isModalThreeOpen}>
-              <div className="datetime-picker">
-                <IonButton
-                  expand="block"
-                  onClick={() => setIsModalThreeOpen(false)}
-                >
-                  Close
-                </IonButton>
-                <IonDatetime
-                  onIonChange={(e) => getTime(e)}
-                  presentation="time"
-                ></IonDatetime>
-              </div>
-            </IonModal>
           </IonItem>
           <IonItem class="ion-add-custom-button">
             <IonButton class="add-custom-button">Submit</IonButton>
