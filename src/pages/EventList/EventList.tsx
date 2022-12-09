9ac866b5f15343a8eb824e6902d25e7fa4e84524
H-isaac23 <dev.isaac23@gmail.com>
2022-12-09 10:58:31 +0800

    finish(?) eventList

--- src/pages/EventList/EventList.tsx
+++ src/pages/EventList/EventList.tsx
@@ -5,12 +5,17 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonCardContent,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
 } from "@ionic/react";
+import { useState } from "react";
 
 import "./EventList.css";
 
 export default function EventList() {
   const test_date = new Date();
+  const [showEventType, setEventType] = useState("major");
   const test_events = [
     {
       eventName: "Event 1",
@@ -41,25 +46,41 @@ export default function EventList() {
       eventType: "minor",
     },
   ];
+
+  const test_func = (e: any) => {
+    setEventType(e.target.value);
+  };
   return (
-    <IonPage>
+    <IonPage className="custom-event-list">
       <IonContent>
-        <div className="cards">
-          {test_events.map((e) => (
-            <IonCard>
-              <IonCardHeader>
-                <IonCardTitle className="ion-text-center">
-                  {e.eventName}
-                </IonCardTitle>
-              </IonCardHeader>
-              <IonCardContent>
-                <p className="ion-text-center">Date: {e.date}</p>
-                <p className="ion-text-center">
-                  Time: {e.startTime} - {e.endTime}
-                </p>
-              </IonCardContent>
-            </IonCard>
-          ))}
+        <IonSegment value={showEventType} onIonChange={(e) => test_func(e)}>
+          <IonSegmentButton value="major">
+            <IonLabel>Major</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="minor">
+            <IonLabel>Minor</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+        <div className="ion-margin-top">
+          {test_events
+            .filter((e) => e.eventType === showEventType)
+            .map((e, i) => (
+              <div key={i}>
+                <IonCard className="ion-margin-start ion-margin-end">
+                  <IonCardHeader>
+                    <IonCardTitle className="ion-text-center">
+                      {e.eventName}
+                    </IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <p className="ion-text-center">Date: {e.date}</p>
+                    <p className="ion-text-center">
+                      Time: {e.startTime} - {e.endTime}
+                    </p>
+                  </IonCardContent>
+                </IonCard>
+              </div>
+            ))}
         </div>
       </IonContent>
     </IonPage>
